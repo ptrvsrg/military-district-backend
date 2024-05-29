d841ff91b00d9f60544fea650dd0d7ae1a568eaf
ptrvsrg <s.petrov1@g.nsu.ru>
2024-05-29 18:52:33 +0700

    Исправил случай, когда сооружение не закреплено за воинской частью

--- db/DML/040_add_report_buildings_by_squad_count.sql
+++ db/DML/040_add_report_buildings_by_squad_count.sql
@@ -8,16 +8,16 @@ VALUES ('Сооружения по количеству дислоцирован
                                                  LEFT JOIN platoons p  ON p.company_id = c.id
                                                  LEFT JOIN squads s    ON s.platoon_id = p.id
                                         GROUP BY u.name)
-        SELECT b.name           AS building_name,
-               uwsc.unit_name   AS unit_name,
-               b.coordinate     AS coordinate,
-               b.address        AS address,
-               uwsc.squad_count AS squad_count
+        SELECT b.name                        AS building_name,
+               uwsc.unit_name                AS unit_name,
+               b.coordinate                  AS coordinate,
+               b.address                     AS address,
+               COALESCE(uwsc.squad_count, 0) AS squad_count
         FROM buildings b
                  LEFT JOIN units_with_squad_count uwsc ON uwsc.unit_name = b.unit_name
         WHERE (b.unit_name = :unitName::VARCHAR            OR :unitName::VARCHAR IS NULL)
-          AND (uwsc.squad_count >= :minSquadCount::INTEGER OR :minSquadCount::INTEGER IS NULL)
-          AND (uwsc.squad_count <= :maxSquadCount::INTEGER OR :maxSquadCount::INTEGER IS NULL)',
+          AND (COALESCE(uwsc.squad_count, 0) >= :minSquadCount::INTEGER OR :minSquadCount::INTEGER IS NULL)
+          AND (COALESCE(uwsc.squad_count, 0) <= :maxSquadCount::INTEGER OR :maxSquadCount::INTEGER IS NULL)',
         ARRAY [
             ('unitName', 'SELECT name FROM units')::REPORT_PARAMETER,
             ('minSquadCount', NULL)::REPORT_PARAMETER,
